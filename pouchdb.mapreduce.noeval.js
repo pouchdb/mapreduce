86192625366769a842c2f7469d89afb7f2d4a4c1
Artur Nowak <artur.nowak@synaway.com>
2013-09-13 13:16:36 +0200

    Embedded `pouch.collate` module
    This module is no longer exposed by the Pouch instance and there is no
    possibility of requiring it from another file in browser environment.
    I added it manually to the source, which of course makes maintanance 2x
    worse, but it seems to be the only valid option.

--- pouchdb.mapreduce.noeval.js
+++ pouchdb.mapreduce.noeval.js
@@ -6,6 +6,86 @@
 "use strict";
 
 var NoEvalMapReduce = function(db) {
+  var pouchCollate = function(a, b) {
+    var ai = collationIndex(a);
+    var bi = collationIndex(b);
+    if ((ai - bi) !== 0) {
+      return ai - bi;
+    }
+    if (a === null) {
+      return 0;
+    }
+    if (typeof a === 'number') {
+      return a - b;
+    }
+    if (typeof a === 'boolean') {
+      return a < b ? -1 : 1;
+    }
+    if (typeof a === 'string') {
+      return stringCollate(a, b);
+    }
+    if (Array.isArray(a)) {
+      return arrayCollate(a, b);
+    }
+    if (typeof a === 'object') {
+      return objectCollate(a, b);
+    }
+  };
+
+  var stringCollate = function(a, b) {
+    // See: https://github.com/daleharvey/pouchdb/issues/40
+    // This is incompatible with the CouchDB implementation, but its the
+    // best we can do for now
+    return (a === b) ? 0 : ((a > b) ? 1 : -1);
+  };
+
+  var objectCollate = function(a, b) {
+    var ak = Object.keys(a), bk = Object.keys(b);
+    var len = Math.min(ak.length, bk.length);
+    for (var i = 0; i < len; i++) {
+      // First sort the keys
+      var sort = pouchCollate(ak[i], bk[i]);
+      if (sort !== 0) {
+        return sort;
+      }
+      // if the keys are equal sort the values
+      sort = pouchCollate(a[ak[i]], b[bk[i]]);
+      if (sort !== 0) {
+        return sort;
+      }
+
+    }
+    return (ak.length === bk.length) ? 0 :
+      (ak.length > bk.length) ? 1 : -1;
+  };
+
+  var arrayCollate = function(a, b) {
+    var len = Math.min(a.length, b.length);
+    for (var i = 0; i < len; i++) {
+      var sort = pouchCollate(a[i], b[i]);
+      if (sort !== 0) {
+        return sort;
+      }
+    }
+    return (a.length === b.length) ? 0 :
+      (a.length > b.length) ? 1 : -1;
+  };
+
+  // The collation is defined by erlangs ordered terms
+  // the atoms null, true, false come first, then numbers, strings,
+  // arrays, then objects
+  var collationIndex = function(x) {
+    var id = ['boolean', 'number', 'string', 'object'];
+    if (id.indexOf(typeof x) !== -1) {
+      if (x === null) {
+        return 1;
+      }
+      return id.indexOf(typeof x) + 2;
+    }
+    if (Array.isArray(x)) {
+      return 4.5;
+    }
+  };
 
   function viewQuery(fun, options) {
     if (!options.complete) {
@@ -64,9 +144,9 @@ var NoEvalMapReduce = function(db) {
         value: val
       };
 
-      if (options.startkey && Pouch.collate(key, options.startkey) < 0) return;
-      if (options.endkey && Pouch.collate(key, options.endkey) > 0) return;
-      if (options.key && Pouch.collate(key, options.key) !== 0) return;
+      if (options.startkey && pouchCollate(key, options.startkey) < 0) return;
+      if (options.endkey && pouchCollate(key, options.endkey) > 0) return;
+      if (options.key && pouchCollate(key, options.key) !== 0) return;
 
       num_started++;
       if (options.include_docs) {
@@ -99,7 +179,7 @@ var NoEvalMapReduce = function(db) {
     var checkComplete = function(){
       if (completed && results.length == num_started){
         results.sort(function(a, b) {
-          return Pouch.collate(a.key, b.key);
+          return pouchCollate(a.key, b.key);
         });
         if (options.descending) {
           results.reverse();
@@ -116,7 +196,7 @@ var NoEvalMapReduce = function(db) {
         var groups = [];
         results.forEach(function(e) {
           var last = groups[groups.length-1] || null;
-          if (last && Pouch.collate(last.key[0][0], e.key) === 0) {
+          if (last && pouchCollate(last.key[0][0], e.key) === 0) {
             last.key.push([e.key, e.id]);
             last.value.push(e.value);
             return;
