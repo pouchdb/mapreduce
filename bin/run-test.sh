309a43fc9a78d60244ad4c695c45fea1dea3c01c
Marten de Vries <marten94@gmail.com>
2015-01-21 17:16:16 +0100

    (#247) - Add PouchDB server to Travis
    Tests fail against PouchDB server, so it's in the allowed failures list.

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -2,8 +2,34 @@
 
 : ${CLIENT:="node"}
 
+if [ "$SERVER" == "pouchdb-server" ]; then
+    mkdir -p ./testdata/server
+    echo '{
+        "httpd": {
+            "port": 5985
+        },
+        "couchdb": {
+            "database_dir": "./testdata/server"
+        },
+        "log": {
+            "file": "./testdata/server/log.txt"
+        }
+    }' > ./testdata/server/config.json
+    ./node_modules/.bin/pouchdb-server -c ./testdata/server/config.json &
+    export POUCHDB_SERVER_PID=$!
+    : ${TEST_DB:="./testdata/client/testdb,http://localhost:5985/testdb"}
+    export TEST_DB
+    sleep 5
+fi
+
 if [ "$CLIENT" == "node" ]; then
     npm run test-node
 else
     npm run test-browser
 fi
+
+EXIT_STATUS=$?
+if [[ ! -z $POUCHDB_SERVER_PID ]]; then
+  kill $POUCHDB_SERVER_PID
+fi
+exit $EXIT_STATUS
